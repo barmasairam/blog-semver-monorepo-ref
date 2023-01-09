3dd269a517f46ff497b95c5c03693e03357d8fb9
barmasairam <sairambarma58@gmail.com>
2023-01-09 12:20:36 +0530

    first test

---
+++ .github/workflows/scripts/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+
+release_type=${1}
+
+version=$(./scripts/version.py get)
+version_file=VERSION
+
+commit=${BITBUCKET_COMMIT:-$(git rev-parse HEAD)}
+branch=${ALLOWED_RELEASE_BRANCH:-master}
+
+if ! git branch -a --contains "${commit}" | grep -e "^[* ]*remotes/origin/${branch}\$"
+then
+  echo -e "###\n### Not on ${branch}. Only ${branch} commits can be released.\n###"
+  exit 1
+else
+  echo -e "###\n### Releasing of ${commit} on ${branch}\n###"
+fi
